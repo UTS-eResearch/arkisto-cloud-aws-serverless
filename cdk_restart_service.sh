402a809d6457dc0cd82ba341d9a373dc2bd400bd
Moises Sacal <moisbo@gmail.com>
2020-12-15 12:14:23 +1100

    removed unused files

--- cdk_restart_service.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-CLUSTER_NAME=SeafoodSafety-arkistocluster4404FD7B-6hfI0bebqopR
-SERVICE_NAME=SeafoodSafety-oniserviceService4ACCF1FB-JLJGkufBBOqY
-
-aws ecs update-service --force-new-deployment --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME}
