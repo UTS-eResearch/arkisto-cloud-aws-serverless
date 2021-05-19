bb6022b1a19a76472e189bf0405d8751dc498cbd
Moises Sacal <moisbo@gmail.com>
2021-05-19 10:30:12 +1000

    changed solr to 8.6.3, added ability to run without oni-indexer, fixed typos

--- lib/oni.ts
+++ lib/oni.ts
@@ -5,6 +5,7 @@ import * as utils from "./utils";
 export default class Oni extends ecs.FargateTaskDefinition {
   oniExpress: any;
   indexer: any;
+  withIndexer: boolean = true;
   solr: any;
   memcached: any;
 
@@ -12,6 +13,9 @@ export default class Oni extends ecs.FargateTaskDefinition {
     super(scope, id, props);
     const base = config.base;
 
+    if(!base.indexer) {
+      this.withIndexer = false;
+    }
     const util = new utils.default();
 
     const expressAssetLocation = util.resolveAsset(base.express.location);
@@ -110,38 +114,40 @@ export default class Oni extends ecs.FargateTaskDefinition {
       protocol: ecs.Protocol.TCP
     });
 
-    const indexerAssetLocation = util.resolveAsset(base.indexer.location);
-    this.indexer = this.addContainer('oni-indexer', {
-      image: ecs.ContainerImage.fromAsset(indexerAssetLocation),
-      memoryLimitMiB: base.indexer.memory, // Default is 512
-      essential: false,
-      logging: config.logging
-    });
-    this.indexer.addMountPoints({
-      sourceVolume: config.ocflVolumeConfig.name,
-      containerPath: '/etc/share/ocfl',
-      readOnly: false,
-    });
-    //this.indexer.addMountPoints(config.logsMount);
-    this.indexer.addMountPoints({
-      sourceVolume: config.configVolumeConfig.name,
-      containerPath: '/etc/share/config',
-      readOnly: false
-    });
-    this.indexer.addPortMappings({
-      containerPort: 8090,
-      hostPort: 8090,
-      protocol: ecs.Protocol.TCP
-    });
-    this.indexer.addUlimits({
-      hardLimit: 65000,
-      softLimit: 65000,
-      name: ecs.UlimitName.CORE,
-    });
-    this.indexer.addContainerDependencies({
-      container: this.oniExpress,
-      condition: ecs.ContainerDependencyCondition.HEALTHY
-    });
+    if(this.withIndexer) {
+      const indexerAssetLocation = util.resolveAsset(base.indexer.location);
+      this.indexer = this.addContainer('oni-indexer', {
+        image: ecs.ContainerImage.fromAsset(indexerAssetLocation),
+        memoryLimitMiB: base.indexer.memory, // Default is 512
+        essential: false,
+        logging: config.logging
+      });
+      this.indexer.addMountPoints({
+        sourceVolume: config.ocflVolumeConfig.name,
+        containerPath: '/etc/share/ocfl',
+        readOnly: false,
+      });
+      //this.indexer.addMountPoints(config.logsMount);
+      this.indexer.addMountPoints({
+        sourceVolume: config.configVolumeConfig.name,
+        containerPath: '/etc/share/config',
+        readOnly: false
+      });
+      this.indexer.addPortMappings({
+        containerPort: 8090,
+        hostPort: 8090,
+        protocol: ecs.Protocol.TCP
+      });
+      this.indexer.addUlimits({
+        hardLimit: 65000,
+        softLimit: 65000,
+        name: ecs.UlimitName.CORE,
+      });
+      this.indexer.addContainerDependencies({
+        container: this.oniExpress,
+        condition: ecs.ContainerDependencyCondition.HEALTHY
+      });
+    }
     // this.logsMount = {
     //   sourceVolume: config.oniLogsVolume,
     //   containerPath: '/etc/share/logs',
